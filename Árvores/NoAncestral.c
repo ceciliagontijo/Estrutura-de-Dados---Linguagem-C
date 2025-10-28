026eeaeb4790ece3d72f6d25ad9407ce5ba41460
Cecília Gontijo V. T. Roque <ceciliagontijo50@gmail.com>
2025-10-27 21:46:04 -0300

    novos programas

---
+++ Árvores/NoAncestral.c
@@ -0,0 +1,176 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct NoArvore
+{
+    int info;
+    struct NoArvore *dir;
+    struct NoArvore *esq;
+}NoArv;
+
+typedef struct Arvore
+{
+    NoArv *raiz;
+}Arv;
+
+Arv* CriaArvore(void)
+{
+    Arv *aux;
+    aux = (Arv*) malloc(sizeof(Arv));
+    aux->raiz = NULL;
+    return aux;
+}
+
+int ArvVazia(Arv *arv)
+{
+    if (arv->raiz == NULL)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+
+}
+
+void insere(Arv *a1, int num)
+{
+    NoArv *novo;
+    NoArv *pai=a1->raiz;
+    int flag=0;
+    novo = (NoArv*) malloc(sizeof(NoArv));
+    novo->info = num;
+    novo->dir = NULL;
+    novo->esq = NULL;
+    if (a1->raiz == NULL)
+    {
+        a1->raiz = novo;
+        return;
+    }
+    else
+    {
+        while (flag==0)
+        {
+            if (num > pai->info)
+            {
+                if (pai->dir != NULL)
+                {
+                    pai = pai->dir;
+                }
+                else
+                {
+                    pai->dir = novo;
+                    flag=1;
+                }
+            }
+            else
+            {
+                if (pai->esq != NULL)
+                {
+                    pai = pai->esq;
+                }
+                else
+                {
+                    pai->esq = novo;
+                    flag=1;
+                }
+            }
+        }
+    }
+}
+
+void imprime(int num)
+{
+    printf("%d ", num);
+}
+
+void Preorder(NoArv *pai)
+{
+    imprime(pai->info);
+    if (pai->dir != NULL)
+    {
+        Preorder(pai->dir);
+    }
+    if (pai->esq != NULL)
+    {
+        Preorder(pai->esq);
+    }
+}
+
+int Busca(NoArv *pai, int num) // recursiva
+{
+    if (pai->info == num)
+    {
+        return 1;
+    }
+    if (num > pai->info)
+    {
+        if (pai->dir != NULL)
+        {
+            return Busca(pai->dir, num);
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    if (num < pai->info)
+    {
+        if (pai->esq != NULL)
+        {
+            return Busca(pai->esq, num);
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
+
+int Ancestral(NoArv *pai, int num)
+{
+    int cont=0;
+    if (Busca(pai, num)==0)
+    {
+        printf("\nO nó não existe.");
+        return 0;
+    }
+    while (pai->info != num)
+    {
+        if (num > pai->info)
+        {
+            cont++;
+            pai = pai->dir;
+        }
+        if (num < pai->info)
+        {
+            cont++;
+            pai = pai->esq;
+        }
+    }
+    return cont;
+}
+
+
+
+int main()
+{
+    Arv *a1;
+    a1 = CriaArvore();
+    int num, valor, no, qnt;
+    printf("Quantidade de elementos: ");
+    scanf("%d", &num);
+    while (num>0)
+    {
+        printf("\nElemento = ");
+        scanf("%d", &valor);
+        insere(a1, valor);
+        num--;
+    }
+    Preorder(a1->raiz);
+    printf("\nNó: ");
+    scanf("%d", &no);
+    qnt = Ancestral(a1->raiz, no);
+    printf("\nQuantidade de ancestrais = %d", qnt);
+}

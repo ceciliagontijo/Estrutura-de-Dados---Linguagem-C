7be3327871f417ef640765dade6c4e3c2d00bf46
Cecília Gontijo V. T. Roque <ceciliagontijo50@gmail.com>
2025-11-06 22:56:46 -0300

    atualizando versão projeto

--- Projeto 2/projeto2.c
+++ Projeto 2/projeto2.c
@@ -2,15 +2,14 @@
 #include <time.h>    // time
 
 #include <stdio.h>
-#include <time.h>
+
 
 typedef struct {
     int chave;
     double valor;
 }item;
 
-
-void geraVetorAleatorio(item vetor[], int tamanho, int sem) {
+void geraCrescente(item vetor[], int tamanho, int sem) {
     srand(sem);
 
     // Gera a primeira chave aleatória entre 100000 e 300000
@@ -25,6 +24,18 @@ void geraVetorAleatorio(item vetor[], int tamanho, int sem) {
 
         // Valor continua aleatório (se quiser, pode seguir outro padrão)
         vetor[i].valor = 100000.0f + ((float)rand() / RAND_MAX) * 200000.0f;
+    }
+}
+
+
+
+
+void geraAleatorio(item v[], int n, int seed) {
+    srand(seed); // usa a mesma semente para repetir o mesmo conjunto
+
+    for (int i = 0; i < n; i++) {
+        v[i].chave = 100000 + rand() % 200001;               // entre 100000 e 300000
+        v[i].valor = 100000.0 + ((double)rand() / RAND_MAX) * 200000.0; // entre 100000 e 300000
     }
 }
 
@@ -49,7 +60,6 @@ void insercao(item *v, int n) {
         }
     }
 }
-
 
 int particao(item *v, int LI, int LS)
 {
@@ -85,6 +95,90 @@ void quicksort(item *v, int LI, int LS)
     }
 }
 
+int particaoLS(item *v, int LI, int LS)
+{
+    int aux, pivo, e = LI, d = LS;
+    pivo = v[d].chave;  // pivô é o último elemento
+
+    while (e < d)
+    {
+        while ((v[e].chave > pivo) && (e < LS))
+        {
+                e++;
+        }
+        while ((v[d].chave <= pivo) && (d > LI))
+        {
+            d--;
+        }
+
+        if (e < d)
+        {
+            aux = v[e].chave;
+            v[e].chave = v[d].chave;
+            v[d].chave= aux;
+        }
+    }
+
+    aux = v[LS].chave;
+    v[LS].chave = v[e].chave;
+    v[e].chave = aux;
+
+    return e;  // retorna a posição do pivô
+}
+
+int particaoM(item *v, int LI, int LS)
+{
+    int aux, pivo, e = LI, d = LS;
+    pivo = v[(LS + LI) / 2].chave;  // pivô central
+
+    while (e < d)
+    {
+        while ((v[e].chave > pivo) && (e < LS))
+        {
+            e++;
+        }
+        while ((v[d].chave < pivo) && (d > LI))
+        {
+                d--;
+        }
+
+        if (e < d)
+        {
+            aux = v[e].chave;
+            v[e].chave = v[d].chave;
+            v[d].chave = aux;
+            e++;
+            d--;
+        }
+    }
+
+    return e;
+}
+
+
+void quicksortLS(item *v, int LI, int LS)
+{
+    if(LI<LS)
+    {
+        int p,i;
+        p = particaoLS(v,LI,LS);
+        quicksortLS(v,LI,p-1);
+        quicksortLS(v,p+1,LS);
+    }
+}
+
+
+void quicksortM(item *v, int LI, int LS)
+{
+    if(LI<LS)
+    {
+        int p,i;
+        p = particaoM(v,LI,LS);
+        quicksortM(v,LI,p-1);
+        quicksortM(v,p+1,LS);
+    }
+}
+
 
 
 int main()
@@ -92,27 +186,21 @@ int main()
     int i;
     int TAM=500000;
     item *vetor = malloc(TAM * sizeof *vetor);
-
     clock_t inicio, fim;
     float tempo_insercao, tempo_quick;
-
-    geraVetorAleatorio(vetor, TAM, 1260);
+    geraAleatorio(vetor, TAM,1260);
     inicio = clock();
-    insercao(vetor,TAM);
+    insercao(vetor, TAM);
     fim = clock();
-    //quicksort(vetor,0,TAM-1);
-    //printf("\n\n\n\n");
-    //for (i = 0; i < TAM; i++) {
-       // printf("Chave %d: %d\n",i, vetor[i].chave);
-    //}
     tempo_insercao = (float)(fim - inicio) / CLOCKS_PER_SEC;
+    /*for (i = 0; i < TAM; i++) {
+        printf("Chave %d: %d\n",i, vetor[i].chave);
+    }*/
 
-    // Exibindo o tempo de execução
-    printf("Tempo de execução: %.6f segundos\n", tempo_insercao);
+    printf("Tempo de execução: %.10f segundos\n", tempo_insercao);
     free(vetor);
     vetor = NULL;
     return 0;
 }
-
 
 
